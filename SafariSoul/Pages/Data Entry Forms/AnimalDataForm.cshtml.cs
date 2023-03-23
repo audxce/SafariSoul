ee759bbeac5a9ffb58ac78e3008d27cfec276d49
audxce <saqures9@CougarNet.UH.EDU>
2023-03-23 16:44:34 -0500

    Created page model for animal data entry form

--- SafariSoul/Pages/Data Entry Forms/AnimalDataForm.cshtml.cs
+++ SafariSoul/Pages/Data Entry Forms/AnimalDataForm.cshtml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,47 @@ namespace SafariSoul.Pages
 {
     public class AnimalData : PageModel
     {
+        [BindProperty]
+        public AnimalData AnimalInput { get; set; }
+
         public void OnGet()
         {
         }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Store the input in the database or any other storage mechanism here
+
+            return RedirectToPage("/Index");
+        }
+    }
+
+    public class AnimaInput
+    {
+        [Required] // part of the System.ComponentModel.DataAnnotations namespace, and it is used to perform server-side validation of user input
+        [StringLength(30)]
+        public string AnimalName { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Genus { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Species { get; set; }
+
+        public string? UniqueDescriptors { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        public string? DietaryRestrictions { get; set; }
     }
 }
