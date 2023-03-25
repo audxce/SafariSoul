2238c73fee5301cfb6b9f3406fb0dcc08389b25b
audxce <saqures9@CougarNet.UH.EDU>
2023-03-24 22:27:23 -0500

    fixed connection error and added data form query for animal to test

--- SafariSoul/Pages/Data Entry Forms/AnimalDataForm.cshtml.cs
+++ SafariSoul/Pages/Data Entry Forms/AnimalDataForm.cshtml.cs
@@ -1,15 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel.DataAnnotations;
+using SafariSoul.Models;
 
 namespace SafariSoul.Pages.Data_Entry_Forms
 {
     public class AnimalData : PageModel
     {
+        private readonly OfficalZooDbContext _context;
+
+        public AnimalData(OfficalZooDbContext context)
+        {
+            _context = context;
+
+        }
+
+        [BindProperty]
+        public Animal Animal { get; set; } = default!;
+
         public void OnGet()
         {
         }
 
-    }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
 
+            _context.Animals.Add(Animal);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/Index");
+        }
+    }
 }
