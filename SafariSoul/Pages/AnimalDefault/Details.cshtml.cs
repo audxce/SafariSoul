8c5d6710529ae18c2771a45258bfb857b32370ab
Nimr0dicus <128177805+Nimr0dicus@users.noreply.github.com>
2023-04-03 21:36:24 -0500

    Updated Indexes
    Updated Index Pages & added default pages

---
+++ SafariSoul/Pages/AnimalDefault/Details.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SafariSoul.Models;
+
+namespace SafariSoul.Pages.AnimalDefault
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly SafariSoul.OfficalZooDbContext _context;
+
+        public DetailsModel(SafariSoul.OfficalZooDbContext context)
+        {
+            _context = context;
+        }
+
+        public Animal Animal { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Animals == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animals.FirstOrDefaultAsync(m => m.AnimalId == id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Animal = animal;
+            }
+            return Page();
+        }
+    }
+}
