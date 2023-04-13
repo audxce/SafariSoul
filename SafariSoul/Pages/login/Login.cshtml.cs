5758c3f3c1f22ee5caa021c644ab82dd8e15d2c0
Nimr0dicus <128177805+Nimr0dicus@users.noreply.github.com>
2023-04-13 18:35:19 -0500

    Got sessions working

--- SafariSoul/Pages/login/Login.cshtml.cs
+++ SafariSoul/Pages/login/Login.cshtml.cs
@@ -3,10 +3,45 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SafariSoul.Pages.Login
 {
-    public class LoginModel : PageModel
+    /*public class LoginModel : PageModel
     {
         public void OnGet()
         {
         }
+    }*/
+
+    public class LoginModel : PageModel
+    {
+        public const string SessionKeyName = "_Name";
+
+        private readonly ILogger<PageModel> _logger;
+
+        public LoginModel(ILogger<PageModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnPost()
+        {
+            var username = Request.Form["Username"];
+
+            if (!String.IsNullOrEmpty(username))
+            {
+                HttpContext.Session.SetString(SessionKeyName, username);
+                //if (await _context.ZooUsers.Where(username == a.UserName))
+                //ZooUsers = await _context.ZooUsers.Where(a => a.UserName==username && a => a.AuthenticationKey==password);
+            }
+        }
+        /*public void OnGet()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(SessionKeyName)))
+            {
+                HttpContext.Session.SetString(SessionKeyName, "Default");
+            }
+            var name = HttpContext.Session.GetString(SessionKeyName);
+
+            _logger.LogInformation("Session Name: {Name}", name);
+        }*/
     }
+
 }
