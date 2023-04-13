4eb7d7423dfa1c7bad77faf6a706492ab81dc21f
Nimr0dicus <128177805+Nimr0dicus@users.noreply.github.com>
2023-04-13 00:00:41 -0500

    Added a session tracker (look at DefaultPage)

--- SafariSoul/Pages/DefaultPage.cshtml.cs
+++ SafariSoul/Pages/DefaultPage.cshtml.cs
@@ -3,10 +3,41 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SafariSoul.Pages.Shared
 {
-    public class DefaultPageModel : PageModel
-    {
-        public void OnGet()
+	public class DefaultPageModel : PageModel
+	{
+		public const string SessionKeyName = "_Name";
+		public const string SessionKeyAge = "_Age";
+
+		private readonly ILogger<DefaultPageModel> _logger;
+
+		public DefaultPageModel(ILogger<DefaultPageModel> logger)
+		{
+			_logger = logger;
+		}
+
+        public async Task OnPost()
         {
+            var username = Request.Form["Username"];
+
+            if (!String.IsNullOrEmpty(username))
+            {
+                HttpContext.Session.SetString(SessionKeyName, username);
+                //if (await _context.ZooUsers.Where(username == a.UserName))
+                //ZooUsers = await _context.ZooUsers.Where(a => a.UserName==username && a => a.AuthenticationKey==password);
+            }
         }
-    }
+        public void OnGet()
+		{
+			if (string.IsNullOrEmpty(HttpContext.Session.GetString(SessionKeyName)))
+			{
+				HttpContext.Session.SetString(SessionKeyName, "The Doctor");
+				HttpContext.Session.SetInt32(SessionKeyAge, 73);
+			}
+			var name = HttpContext.Session.GetString(SessionKeyName);
+			var age = HttpContext.Session.GetInt32(SessionKeyAge).ToString();
+
+			_logger.LogInformation("Session Name: {Name}", name);
+			_logger.LogInformation("Session Age: {Age}", age);
+		}
+	}
 }
--- SafariSoul/Program.cs
+++ SafariSoul/Program.cs
@@ -5,7 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();     //new
 builder.Services.AddDbContext<ZooDbContext>(options => options.UseMySql(builder.Configuration["ConnectionStrings:DefaultConnection"],ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])));
+
+builder.Services.AddDistributedMemoryCache();   //new
+
+builder.Services.AddSession(options =>  //new
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
@@ -24,7 +34,10 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseSession();   //new
+
 app.MapRazorPages();
+app.MapDefaultControllerRoute();    //new
 
 app.Run();
 
