19258da5dca15241189863c44d107062926971d4
bbqcandy <114030730+bbqcandy@users.noreply.github.com>
2023-04-13 21:41:30 -0500

    Updated Login and IT WORKS (almost)
    Update works and recognizes what kind of user is logging in. Redirect not working yet tho.

--- SafariSoul/Pages/login/Login.cshtml.cs
+++ SafariSoul/Pages/login/Login.cshtml.cs
@@ -11,37 +11,45 @@ namespace SafariSoul.Pages.Login
 {
     public class LoginModel : PageModel
     {
-        public IActionResult OnPostc()
+        public IActionResult OnPost()
         {
-            string connectionString = "Server = zoo - db - server.mysql.database.azure.com; UserID = audace; Password = '37PE&CWYy9e@'; Database = zoo_db;";
+            string connectionString = "Server=zoo-db-server.mysql.database.azure.com;UserID=audace;Password='37PE&CWYy9e@';Database=zoo_db;";
             MySqlConnection connection = new MySqlConnection(connectionString);
 
+            // Get the username and password from the form
             string username = Request.Form["username"];
             string password = Request.Form["password"];
 
+            // Check if the username and password are valid
+            connection.Open();
+
+            // Create a command to check if the username and password are valid
             MySqlCommand command = new MySqlCommand("SELECT * FROM zoo_user WHERE User_Name=@username AND Authentication_Key=@password", connection);
             command.Parameters.AddWithValue("@username", username);
             command.Parameters.AddWithValue("@password", password);
 
+            // Execute the command
+            command.ExecuteNonQuery();
+
+            // Get the result of the command
             MySqlDataReader reader = command.ExecuteReader();
 
+            // Check if the username and password are valid
             if (reader.Read())
             {
+                // Get the user type
                 string userType = reader.GetString("User_Type");
-
                 switch (userType)
                 {
+                    // since we are still testing, we will just show a message that confirms what type of user is logged in
                     case "Admin":
-                        return RedirectToPage("Admin View");
+                        return Content($"Admin Logged in");
                     case "Employee":
-                        return RedirectToPage("Employee View");
+                        return Content($"Employee Logged in");
                     case "Customer":
-                        return RedirectToPage("Customer View");
-                    case "Default":
-                        return RedirectToPage("Default View");
+                        return Content($"Customer Logged in");
                     default:
-                        ModelState.AddModelError(string.Empty, "Invalid user type.");
-                        break;
+                        return Content($"Default Logged in");
                 }
             }
             else
