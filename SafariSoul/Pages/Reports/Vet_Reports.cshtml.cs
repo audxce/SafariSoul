58fc7d81f9904cb80f53319f44396316e65deaea
ReginaldGreen02 <115752371+ReginaldGreen02@users.noreply.github.com>
2023-04-14 19:10:41 -0500

    Vet C#-side done; Just gotta get the main pull

--- SafariSoul/Pages/Reports/Vet_Reports.cshtml.cs
+++ SafariSoul/Pages/Reports/Vet_Reports.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySqlConnector;
 
 namespace SafariSoul.Pages.Reports
 {
@@ -7,6 +8,98 @@ namespace SafariSoul.Pages.Reports
     {
         public void OnGet()
         {
+        }
+        public IActionResult OnGetIncom()
+        {
+            List<VetinarianReports> vetReports = new List<VetinarianReports>();
+
+            using (MySqlConnection connection = new MySqlConnection("Server=zoo-db-server.mysql.database.azure.com;UserID=audace;Password='37PE&CWYy9e@';Database=zoo_db;"))
+            {
+                connection.Open();
+                using (MySqlCommand command = new MySqlCommand(@"SELECT (case when V.animal = A.Animal_ID then A.Animal_Name else 0 end) As Animal_Name, 
+		                                                        V.Vet_Visit_ID,V.Urgency, V.Animal_Condition
+                                                        FROM  veterinary_visit V, animal A
+                                                        WHERE V.animal = A.Animal_ID  and V.Time_Discharged IS NULL
+                                                        ORDER BY V.Urgency;", connection))
+                {
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            VetinarianReports vetinarianReports = new VetinarianReports();
+                            vetinarianReports.Animal_Name = reader["Location"].ToString();
+                            vetinarianReports.Vet_Visit_ID = Convert.ToInt32(reader["Ticket_Num"]);
+                            vetinarianReports.Animal_Condition = reader["Details"].ToString();
+                            vetinarianReports.Urgency = Convert.ToInt32(reader["Urgency"]);
+                            //vetinarianReports.Time_Requested = Convert.ToDateTime(reader["Requested_at"]);
+
+                            vetReports.Add(vetinarianReports);
+                        }
+
+                    }
+
+
+
+
+                }
+
+                ViewData["VetReportIncompleted"] = vetReports;
+
+                return Page();
+            }
+
+        }
+
+        public IActionResult OnGetCom()
+        {
+            List<VetinarianReports> vetReports = new List<VetinarianReports>();
+
+            using (MySqlConnection connection = new MySqlConnection("Server=zoo-db-server.mysql.database.azure.com;UserID=audace;Password='37PE&CWYy9e@';Database=zoo_db;"))
+            {
+                connection.Open();
+                using (MySqlCommand command = new MySqlCommand(@"SELECT (case when V.animal = A.Animal_ID then A.Animal_Name else 0 end) As Animal_Name, 
+		                                                        V.Vet_Visit_ID,V.Urgency, V.Animal_Condition
+                                                        FROM  veterinary_visit V, animal A
+                                                        WHERE V.animal = A.Animal_ID  and V.Time_Discharged IS NULL
+                                                        ORDER BY V.Urgency;", connection))
+                {
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            VetinarianReports vetinarianReports = new VetinarianReports();
+                            vetinarianReports.Animal_Name = reader["Location"].ToString();
+                            vetinarianReports.Vet_Visit_ID = Convert.ToInt32(reader["Ticket_Num"]);
+                            vetinarianReports.Animal_Condition = reader["Details"].ToString();
+                            vetinarianReports.Urgency = Convert.ToInt32(reader["Urgency"]);
+                            //vetinarianReports.Time_Requested = Convert.ToDateTime(reader["Requested_at"]);
+
+                            vetReports.Add(vetinarianReports);
+                        }
+
+                    }
+
+
+
+
+                }
+
+                ViewData["VetReportCompleted"] = vetReports;
+
+                return Page();
+            }
+
+        }
+
+        public class VetinarianReports
+        {
+            public string Animal_Name { get; set; }
+            public int Vet_Visit_ID { get; set; }
+            public int Urgency { get; set; }
+            public string Animal_Condition { get; set; }
+
         }
     }
 }
