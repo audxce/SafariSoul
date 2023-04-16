7d12b04fe1dcc4112b992484c2b502ec6e3d9e3f
Nimr0dicus <128177805+Nimr0dicus@users.noreply.github.com>
2023-04-15 21:30:03 -0500

    Fixed all the layouts

---
+++ SafariSoul/Pages/Shared/_LayoutHumanResources.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SafariSoul.Pages.Shared
+{
+    public class _LayoutHumanResourcesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
